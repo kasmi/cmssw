d26d9a2e34b653d441e01ffece4866d5f48ec1ac
Azeddine <kasmi@fnal.gov>
2013-11-15 21:19:27 -0600

    rel val SLHC

--- Validation/HcalRecHits/python/HcalRecHitParam_cfi.py
+++ Validation/HcalRecHits/python/HcalRecHitParam_cfi.py
@@ -3,14 +3,19 @@ import FWCore.ParameterSet.Config as cms
 hcalRecoAnalyzer = cms.EDAnalyzer("HcalRecHitsValidation",
     outputFile                = cms.untracked.string('HcalRecHitValidationRelVal.root'),
 
+### Upgrade 2019
+#    HBHERecHitCollectionLabel = cms.untracked.InputTag("hbheUpgradeReco"),
+#    HFRecHitCollectionLabel   = cms.untracked.InputTag("hfUpgradeReco"),
+### 2017
     HBHERecHitCollectionLabel = cms.untracked.InputTag("hbhereco"),
     HFRecHitCollectionLabel   = cms.untracked.InputTag("hfreco"),
+#####
     HORecHitCollectionLabel   = cms.untracked.InputTag("horeco"),
-
     eventype                  = cms.untracked.string('multi'),
     ecalselector              = cms.untracked.string('yes'),
     hcalselector              = cms.untracked.string('all'),
-    mc                        = cms.untracked.string('no')
+    mc                        = cms.untracked.string('no'),
+    doSLHC                    = cms.untracked.bool(False) #  True for SLHC and False for regular rel val 
 )
 
 hcalNoiseRates = cms.EDAnalyzer('NoiseRates',
